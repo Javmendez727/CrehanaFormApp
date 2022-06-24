c6f107ca61a85d128deccfef783b34faf9c42abf
Jose Vallejo <jose.vallejo@kushkipagos.com>
2022-06-24 02:05:35 -0500

    [FA-001] Implementation App logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+interface EmpresaObject {
+  priceLMS: number;
+  priceContenido: number;
+  priceEngagement: number;
+  pricePerformance: number;
+}
 
 function App() {
+  const [preciosBaseEmpresa, setPreciosBaseEmpresa] = useState<EmpresaObject>();
+
+  function selectEmpresa(event: React.FormEvent<HTMLSelectElement>): void {
+    if(event.currentTarget.value === "Small Business"){
+      setPreciosBaseEmpresa({
+        priceLMS: 54,
+        priceContenido: 100,
+        priceEngagement: 31,
+        pricePerformance: 41,
+      });
+    }
+    else{
+      setPreciosBaseEmpresa({
+        priceLMS: 69,
+        priceContenido: 115,
+        priceEngagement: 31,
+        pricePerformance: 41,
+      });
+    }
+    console.log(preciosBaseEmpresa)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="boxDropdownInput">
+          <div className="dropdownInput">
+            <h1>Empresa</h1>
+            <select
+              className="combo-box"
+              name="select"
+              onClick={selectEmpresa}
+            >
+              <option disabled selected>
+                Seleccionar
+              </option>
+              <option value="value1">Small Business</option>
+              <option value="value2">Mid Market o Enterprise</option>
+            </select>
+          </div>
+          <div className="dropdownInput">
+            <h1>Cantidad Persona</h1>
+
+            <select className="combo-box" name="select">
+              <option disabled selected>
+                Seleccionar
+              </option>
+              <option value="value1">Small Business</option>
+              <option value="value2">Mid Market o Enterprise</option>
+            </select>
+          </div>
+          <div className="dropdownInput">
+            <h1>Tiempo</h1>
+
+            <select className="combo-box" name="select">
+              <option disabled selected>
+                Seleccionar
+              </option>
+              <option value="value1">Small Business</option>
+              <option value="value2">Mid Market o Enterprise</option>
+            </select>
+          </div>
+        </div>
+        <div className="boxCheckboxInputs">
+          <div className="checkboxStyle">
+            <input type="checkbox" />
+            <label> LMS </label>
+          </div>
+          <div className="checkboxStyle">
+            <input type="checkbox" />
+            <label> Contenido </label>
+          </div>
+          <div className="checkboxStyle">
+            <input type="checkbox" />
+            <label> Engagement </label>
+          </div>
+          <div className="checkboxStyle">
+            <input type="checkbox" />
+            <label> Performance </label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
